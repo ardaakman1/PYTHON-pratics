f72b6e1decee78afbc2397382ab592cd5846ae73
Arda Akman <ardaakman3701@gmail.com>
2026-03-22 21:47:18 +0300

    big fix has been made

--- 04Shopping_list(write)/Shopping_list.py
+++ 04Shopping_list(write)/Shopping_list.py
@@ -7,7 +7,7 @@ if len(sys.argv) != 2:
 print()
 number = int(input("How many items that you want to buy?"))
 with open (sys.argv[1], "w", newline = '') as file: 
-    #  Prevents extra blank lines in CSV files on windows
+    # newline ='' Prevents extra blank lines in CSV files on windows
     headers = ["item", "count"] #  the headers of the CSV
     writer = csv.DictWriter(file, fieldnames=headers) # writer is my variable 
     #  fieldnames is an argument it helps us to define our headers to writer
@@ -16,7 +16,7 @@ with open (sys.argv[1], "w", newline = '') as file:
         name = input(f"please enter the item {i + 1}    ")
         amount = int(input(f"please enter the amount of the item {i + 1}    "))
         row_to_save = {"item" : name, "count" : amount} #  row_to_save is a variable
-        #  we use : unless = because we define dictionaries with : 
+        #  we use ':' instead of '=' because dictionaries are defined with colons
         #  CRITIC: Create a dictionary to map user input to the CSV fieldnames
         writer.writerow(row_to_save)
 print(f"\n\nProcess is completed\n{sys.argv[1]} is created succesfully")
